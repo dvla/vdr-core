b33e6b00c3177d009274aad637e8aed6d54035f0
Ronan Murray <ronanmurray@gmail.com>
2013-03-20 17:49:43 +0000

    updated datastore to retrieve by details

--- common-service/src/main/java/uk/gov/dvla/services/common/ServiceDateFormat.java
+++ common-service/src/main/java/uk/gov/dvla/services/common/ServiceDateFormat.java
@@ -5,6 +5,6 @@ import java.text.SimpleDateFormat;
 
 public final class ServiceDateFormat
 {
-    private static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
+    public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
     public static final DateFormat DEFAULT = new SimpleDateFormat(DEFAULT_DATE_FORMAT);
 }
