63159a6a9109322dbb9ae7df5044d8bada11a1a5
Peter van der Merwe <peter@vandermerwe.co.za>
2013-04-16 13:50:03 +0100

    Added in a message blaster to tst performance, cleaned up a bit of the code

--- messages/src/main/scala/uk/gov/dvla/messages/AuditRecords.scala
+++ messages/src/main/scala/uk/gov/dvla/messages/AuditRecords.scala
@@ -1,5 +1,11 @@
 package uk.gov.dvla.messages
 
-case class UserSuccessful(dln : String, result : String)
-case class UserUnsuccessful(dln : String, result : String)
-case class DvlaUserUnsuccessful(dln : String, result : String)
+import java.util.UUID
+
+trait Message {
+  val messageId: UUID = java.util.UUID.randomUUID
+}
+
+case class UserSuccessful(dln : String, result : String) extends Message
+case class UserUnsuccessful(dln : String, result : String) extends Message
+case class DvlaUserUnsuccessful(dln : String, result : String) extends Message
