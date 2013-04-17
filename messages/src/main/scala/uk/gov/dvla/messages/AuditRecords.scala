dc3d0976f927288cec92e5481fc6ed1f951947fe
Peter van der Merwe <peter@vandermerwe.co.za>
2013-04-17 11:45:10 +0100

    Figure out the name of the message

--- messages/src/main/scala/uk/gov/dvla/messages/AuditRecords.scala
+++ messages/src/main/scala/uk/gov/dvla/messages/AuditRecords.scala
@@ -1,10 +1,6 @@
 package uk.gov.dvla.messages
 
-import java.util.UUID
-
-trait Message {
-  val messageId: UUID = java.util.UUID.randomUUID
-}
+import uk.gov.dvla.servicebus.core.Message
 
 case class UserSuccessful(dln : String, result : String) extends Message
 case class UserUnsuccessful(dln : String, result : String) extends Message
