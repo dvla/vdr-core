4a671e687acaa29faf92456c2294cb4e2a9fba65
Meirion Steele <meirion.steele@gmail.com>
2013-05-13 15:57:14 +0100

    end of monday

--- api/src/main/java/uk/gov/dvla/services/filter/FilterService.java
+++ api/src/main/java/uk/gov/dvla/services/filter/FilterService.java
@@ -1,8 +1,9 @@
 package uk.gov.dvla.services.filter;
 
 import uk.gov.dvla.domain.ServiceResult;
+import java.util.HashMap;
 
 public interface FilterService<T>
 {
-    public ServiceResult<T> filter(T objectToFilter);
+    public ServiceResult<T> filter(T objectToFilter, HashMap<String, Object> otherValues);
 }
