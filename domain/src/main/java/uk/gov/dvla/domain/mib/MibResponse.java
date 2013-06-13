a61f4dcc3c97e674cb2b0f1576fcf4f22c39ee02
Ruairi Quinn <ruairiquinn@hotmail.com>
2013-06-13 10:44:57 +0100

    Interim commit, beginning of transform service

--- domain/src/main/java/uk/gov/dvla/domain/mib/MibResponse.java
+++ domain/src/main/java/uk/gov/dvla/domain/mib/MibResponse.java
@@ -1,29 +1,43 @@
 package uk.gov.dvla.domain.mib;
 
-import uk.gov.dvla.domain.Licence;
-import uk.gov.dvla.domain.Person;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import uk.gov.dvla.domain.*;
 
+import java.util.Date;
 import java.util.List;
 
 public class MibResponse {
 
-    public class Driver extends Person {
+    private MibDriver driver;
+    private List<Message> messages;
+    private String ruleApplied;
 
-        private Licence licence;
+    public MibResponse(){};
+
+    public MibResponse(Driver driver) {
+        this.driver = new MibDriver(driver);
+    }
+
+    public class MibDriver {
+
+        private MibLicence licence;
         private List<Integer> stopMarker;
         private List<Integer> restrictionKey;
         private List<String> caseType;
         private List<String> errorCode;
-        private Boolean endorsementAmountExcess;
         private String statusCode = null;
 
-        private String currentDriverNumber = null;
+        public MibDriver(){};
 
-        public void setLicence(Licence lics) {
-            licence = lics;
+        public MibDriver(Driver driver) {
+            licence = new MibLicence(driver.getLicence().get(0));
+            stopMarker = driver.getStopMarker();
+            restrictionKey = driver.getRestrictionKey();
+            caseType = driver.getCaseType();
+            errorCode = driver.getErrorCode();
         }
 
-        public Licence getLicence() {
+        public MibLicence getLicence() {
             return this.licence;
         }
 
@@ -59,14 +73,6 @@ public class MibResponse {
             this.errorCode = errorCodes;
         }
 
-        public void setCurrentDriverNumber(String dln) {
-            this.currentDriverNumber = dln;
-        }
-
-        public String getCurrentDriverNumber() {
-            return this.currentDriverNumber;
-        }
-
         public String getStatusCode() {
             return statusCode;
         }
@@ -74,15 +80,216 @@ public class MibResponse {
         public void setStatusCode(String statusCode) {
             this.statusCode = statusCode;
         }
+    }
 
-        public Boolean getEndorsementAmountExcess() {
-            return endorsementAmountExcess;
+    public class MibLicence {
+
+        public Date validFrom;
+        public Date validTo;
+        public String status;
+        public Integer directiveStatus;
+        private List<Entitlement> entitlements;
+        private List<Endorsement> endorsements;
+
+        public MibLicence(){};
+
+        public MibLicence(Licence licence) {
+//            this.driver = new MibDriver(driver);
         }
 
-        public void setEndorsementAmountExcess(Boolean endorsmentAmountExcess) {
-            this.endorsementAmountExcess = endorsmentAmountExcess;
+        public void setDirectiveStatus(Integer directiveStatus) {
+            this.directiveStatus = directiveStatus;
+        }
+
+        public Integer getDirectiveStatus() {
+            return directiveStatus;
+        }
+
+        public Date getValidFrom() {
+            return validFrom;
+        }
+
+        public void setValidFrom(Date validFrom) {
+            this.validFrom = validFrom;
+        }
+
+        public Date getValidTo() {
+            return validTo;
+        }
+
+        public void setValidTo(Date validTo) {
+            this.validTo = validTo;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public List<Entitlement> getEntitlements() {
+            return entitlements;
+        }
+
+        public void setEntitlements(List<Entitlement> entitlements) {
+            this.entitlements = entitlements;
+        }
+
+        public List<Endorsement> getEndorsements() {
+            return endorsements;
+        }
+
+        public void setEndorsements(List<Endorsement> endorsements) {
+            this.endorsements = endorsements;
+        }
+    }
+
+    public class MibEntitlement {
+
+        private String code;
+        private Date validFrom;
+        private Date validTo;
+        private Boolean isProvisional = null;
+        private List<EntitlementRestriction> restrictions;
+        private TestPassStatus testPassStatus;
+        private Date datePassed;
+
+        public String getCode() {
+            return code;
+        }
+
+        public void setCode(String code) {
+            this.code = code;
+        }
+
+        public Date getValidFrom() {
+            return validFrom;
+        }
+
+        public void setValidFrom(Date validFrom) {
+            this.validFrom = validFrom;
+        }
+
+        public Date getValidTo() {
+            return validTo;
+        }
+
+        public void setValidTo(Date validTo) {
+            this.validTo = validTo;
+        }
+
+        public Boolean getIsProvisional() {
+            return isProvisional;
+        }
+
+        public void setIsProvisional(Boolean provisional) {
+            this.isProvisional = provisional;
+        }
+
+        public List<EntitlementRestriction> getRestrictions() {
+            return restrictions;
+        }
+
+        public void setRestrictions(List<EntitlementRestriction> restrictions) {
+            this.restrictions = restrictions;
+        }
+
+        // Calculated field used to simplify data sent to the MIB
+        @JsonProperty("entitlementType")
+        public EntitlementType getEntitlementType() {
+            EntitlementType entitlementType = EntitlementType.Full;
+
+            if (isProvisional) {
+                if (testPassStatus == TestPassStatus.Unclaimed) {
+                    entitlementType = EntitlementType.UnclaimedTestPass;
+                }else {
+                    entitlementType = EntitlementType.Provisional;
+                }
+            }
+
+            return entitlementType;
+        }
+    }
+
+    public class MibEndorsement {
+
+        public Boolean isDisqualification;
+        public String offenceCode;
+        public Date offenceDate;
+
+        //Disqualification Only
+        public Date convictionDate;
+        public Date sentencingDate;
+        public String period;
+        public double fine;
+
+        //Penalty Points Only
+        public Integer numberOfPoints;
+
+        public Boolean getDisqualification() {
+            return isDisqualification;
+        }
+
+        public void setDisqualification(Boolean disqualification) {
+            isDisqualification = disqualification;
+        }
+
+        public String getOffenceCode() {
+            return offenceCode;
+        }
+
+        public void setOffenceCode(String offenceCode) {
+            this.offenceCode = offenceCode;
+        }
+
+        public Date getOffenceDate() {
+            return offenceDate;
+        }
+
+        public void setOffenceDate(Date offenceDate) {
+            this.offenceDate = offenceDate;
+        }
+
+        public Date getConvictionDate() {
+            return convictionDate;
+        }
+
+        public void setConvictionDate(Date convictionDate) {
+            this.convictionDate = convictionDate;
+        }
+
+        public Date getSentencingDate() {
+            return sentencingDate;
+        }
+
+        public void setSentencingDate(Date sentencingDate) {
+            this.sentencingDate = sentencingDate;
+        }
+
+        public String getPeriod() {
+            return period;
+        }
+
+        public void setPeriod(String period) {
+            this.period = period;
+        }
+
+        public Number getFine() {
+            return fine;
+        }
+
+        public void setFine(double fine) {
+            this.fine = fine;
+        }
+
+        public Integer getNumberOfPoints() {
+            return numberOfPoints;
+        }
+
+        public void setNumberOfPoints(Integer numberOfPoints) {
+            this.numberOfPoints = numberOfPoints;
         }
     }
 }
-
-
