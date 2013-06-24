bf4cbc06f9cff03dc18b67ca8127220ef0d52faf
Ruairi Quinn <ruairiquinn@hotmail.com>
2013-06-24 14:57:31 +0100

    Rename MibResponse to MibDTO, and move transform service inteface and mib implementation into suitable packages

---
+++ common-service/src/main/java/uk/gov/dvla/services/transform/MibDriverTransformService.java
@@ -0,0 +1,106 @@
+package uk.gov.dvla.services.transform;
+
+import uk.gov.dvla.domain.*;
+import uk.gov.dvla.domain.mib.EntitlementType;
+import uk.gov.dvla.domain.mib.MibDTO;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class MibDriverTransformService implements TransformService<ServiceResult<Driver>, ServiceResult<MibDTO>> {
+
+    @Override
+    public ServiceResult<MibDTO> transform(ServiceResult<Driver> objectToTransform) {
+        MibDTO mibDTO = new MibDTO();
+        mibDTO.setDriver(new MibDTO.Driver());
+        Driver driver = objectToTransform.getResult();
+        Licence licence = driver.getLicence().get(0);
+
+        MibDTO.Driver mibDriver = mibDTO.getDriver();
+
+        mibDriver.setLicence(getLicence(licence));
+        MibDTO.Licence mibLicence = mibDriver.getLicence();
+        mibLicence.setEntitlements(getEntitlements(licence));
+        mibLicence.setEndorsements(getEndorsements(licence));
+
+        return new ServiceResult<MibDTO>(objectToTransform.getEnquiryId(), mibDTO, objectToTransform.getMessages());
+    }
+
+    private MibDTO.Licence getLicence(Licence licence) {
+
+        MibDTO.Licence mibLicence = new MibDTO.Licence();
+        mibLicence.setValidFrom(licence.getValidFrom());
+        mibLicence.setValidTo(licence.getValidTo());
+        mibLicence.setDirectiveStatus(licence.getDirectiveStatus());
+
+        return mibLicence;
+    }
+
+    private List<MibDTO.Entitlement> getEntitlements(Licence licence) {
+        List<MibDTO.Entitlement> entitlements = new ArrayList<MibDTO.Entitlement>();
+
+        for (Entitlement ent : licence.getEntitlements()) {
+            MibDTO.Entitlement mibEntitlement = new MibDTO.Entitlement();
+            mibEntitlement.setCode(ent.getCode());
+            mibEntitlement.setValidFrom(getValidFrom(ent));
+            mibEntitlement.setValidTo(getValidTo(ent));
+            mibEntitlement.setType(getEntitlementType(ent));
+
+            entitlements.add(mibEntitlement);
+        }
+
+        return entitlements;
+    }
+
+    private List<MibDTO.Endorsement> getEndorsements(Licence licence) {
+        List<MibDTO.Endorsement> entitlements = new ArrayList<MibDTO.Endorsement>();
+
+        for (Endorsement end : licence.getEndorsements()) {
+            MibDTO.Endorsement mibEndorsement = new MibDTO.Endorsement();
+            mibEndorsement.setCode(end.getOffenceCode());
+            mibEndorsement.setOffenceDate(end.getOffenceDate());
+            mibEndorsement.setConvictionDate(end.getConvictionDate());
+            mibEndorsement.setSentencingDate(end.getSentencingDate());
+            mibEndorsement.setFine(end.getFine());
+            mibEndorsement.setNoOfPoints(end.getNumberOfPoints());
+            mibEndorsement.setIsDisqual(end.getDisqualification());
+            mibEndorsement.setDisqualPeriod(end.getPeriod());
+
+            entitlements.add(mibEndorsement);
+        }
+
+        return entitlements;
+    }
+
+    private EntitlementType getEntitlementType(Entitlement ent) {
+        EntitlementType entitlementType = EntitlementType.Full;
+
+        if (ent.getIsProvisional()) {
+            if (ent.isUnclaimedTestPass()) {
+                entitlementType = EntitlementType.UnclaimedTestPass;
+            }else {
+                entitlementType = EntitlementType.Provisional;
+            }
+        }
+
+        return entitlementType;
+    }
+
+    private Date getValidFrom(Entitlement entitlement) {
+        Date validFrom = entitlement.getValidFrom();
+
+        if (entitlement.getIsProvisional() && entitlement.isUnclaimedTestPass()) {
+            validFrom = entitlement.getDatePassed();
+        }
+
+        return validFrom;
+    }
+
+    private Date getValidTo(Entitlement entitlement) {
+        Date validTo = entitlement.getValidTo();
+        Date expiryDate = entitlement.getUnclaimedTestPassExpiryDate();
+
+        return (expiryDate == null) ? validTo : expiryDate;
+    }
+}
