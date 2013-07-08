a44cba3047056b61a3f582c59301a4044255012e
Meirion Steele <meirion.steele@gmail.com>
2013-07-08 13:47:07 +0100

    audit

--- common-service/src/main/java/uk/gov/dvla/services/common/PostcodeHelper.java
+++ common-service/src/main/java/uk/gov/dvla/services/common/PostcodeHelper.java
@@ -7,6 +7,7 @@ public class PostcodeHelper {
 
     /**
      * Checks if a postcode contains any non-alpha characters
+     *
      * @param postcodeToCheck The postcode you need to check
      * @return True if actualPostcode contains non-alpha characters
      */
@@ -15,11 +16,16 @@ public class PostcodeHelper {
         return p.matcher(cleanPostcode(postcodeToCheck)).find();
     }
 
+    public static boolean isBlank(String postcodeToCheck) {
+        return (postcodeToCheck.equals(null) || postcodeToCheck.isEmpty());
+    }
+
     /**
      * Checks to see if the postcode on record matches the user submitted postcode
-     * @param actualPostcode The postcode on the driver record
+     *
+     * @param actualPostcode   The postcode on the driver record
      * @param searchedPostcode The postcode searched for by the user
-     * @param dummyPostcodes  A list of dummy postcodes to ignore
+     * @param dummyPostcodes   A list of dummy postcodes to ignore
      * @return True if the postcodes match, false if they do not or actualPostcode contained a value in dummyPostcodes
      */
     public static boolean postcodeMismatch(String actualPostcode, String searchedPostcode, List<String> dummyPostcodes) {
@@ -27,9 +33,10 @@ public class PostcodeHelper {
         String cleanSearchPostcode = cleanPostcode(searchedPostcode);
         String cleanActualPostcode = cleanPostcode(actualPostcode);
 
-        if (cleanActualPostcode.equals(null) || cleanActualPostcode.isEmpty()) {
-            return false;//Ignore postcode match if blank
+        if (PostcodeHelper.isBlank(cleanActualPostcode)) {
+            return false;
         }
+
         if (PostcodeHelper.hasSpecialChars(cleanActualPostcode)) {
             return false; //Ignore postcode match if it contains non postcode chars
         }
@@ -46,6 +53,6 @@ public class PostcodeHelper {
     }
 
     private static String cleanPostcode(String originalPostcode) {
-        return  originalPostcode.replace(" ","");
+        return originalPostcode.replace(" ", "");
     }
 }
