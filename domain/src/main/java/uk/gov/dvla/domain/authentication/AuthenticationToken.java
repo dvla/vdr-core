5f5f04562712c6af0686a05f581a712c05e6aba1
Ruairi Quinn <ruairiq@toast.local>
2013-07-24 13:33:36 +0100

    Interim commit, trying to get resource files for dummy service

--- domain/src/main/java/uk/gov/dvla/domain/authentication/AuthenticationToken.java
+++ domain/src/main/java/uk/gov/dvla/domain/authentication/AuthenticationToken.java
@@ -2,7 +2,8 @@ package uk.gov.dvla.domain.authentication;
 
 public abstract class AuthenticationToken
 {
-    protected boolean valid_i = false;
+    private boolean valid_i = false;
+    private String token;
 
     public boolean getIsValid()
     {
@@ -12,5 +13,13 @@ public abstract class AuthenticationToken
     public void setIsValid(Boolean isValid)
     {
         this.valid_i = isValid;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
     }
 }
