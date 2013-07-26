75e88a2d29537357108a4e1e014fd6d983df3912
Ruairi Quinn <ruairiq@toast.local>
2013-07-26 15:01:41 +0100

    Update AuthService in CustomerPortal to use the AuthServer for authentication.  Update tests and some minor refactoring to use new QueryStringParameters class

--- api/src/main/java/uk/gov/dvla/services/authentication/AuthenticationProvider.java
+++ api/src/main/java/uk/gov/dvla/services/authentication/AuthenticationProvider.java
@@ -10,13 +10,6 @@ import java.util.List;
 public interface AuthenticationProvider<A extends AuthenticationToken> extends ManagedService
 {
     public static final String AUTHENTICATION_URI = "/auth/";
-    public static final String DLN_SEARCH = "/dln/";
-    public static final String DETAILS_SEARCH = "/details/";
-    public static final String DLN_PARAM = "dln";
-    public static final String SURNAME_PARAM = "sn";
-    public static final String DOB_PARAM = "d";
-    public static final String NINO_PARAM = "n";
-    public static final String POSTCODE_PARAM = "p";
 
     public List<A> authenticate(A authToken) throws AuthenticationException;
 }
