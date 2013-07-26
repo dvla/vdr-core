bc5cd7cc970c461d0c353151d74dee1eb04c67a9
Ruairi Quinn <ruairiq@toast.local>
2013-07-26 15:01:41 +0100

    Update AuthService in CustomerPortal to use the AuthServer for authentication.  Update tests and some minor refactoring to use new QueryStringParameters class

---
+++ core/src/main/java/uk/gov/dvla/core/QueryStringParameters.java
@@ -0,0 +1,19 @@
+package uk.gov.dvla.core;
+
+public class QueryStringParameters {
+
+    public static final String DLN_PARAM = "dln";
+    public static final String FORENAME_PARAM = "fn";
+    public static final String SURNAME_PARAM = "sn";
+    public static final String DOB_PARAM = "d";
+    public static final String NINO_PARAM = "n";
+    public static final String POSTCODE_PARAM = "p";
+    public static final String GENDER_PARAM = "g";
+    public static final String SERVICE_TYPE_PARAM = "type";
+    public static final String CONTACT_CHANNEL_PARAM = "cc";
+    public static final String ENQUIRY_REASON_PARAM = "er";
+    public static final String ENQUIRY_ID_PARAM = "id";
+
+    public static final String DLN_SEARCH_PATH = "/dln/";
+    public static final String PERSONAL_DETAILS_SEARCH_PATH = "/details/";
+}
