1722b7d8619540243df2717e5bc42e4e26620d49
Ben Powell <blpowell@gmail.com>
2013-07-31 15:21:55 +0100

    Added checksum code from origin branch

---
+++ core/src/main/java/uk/gov/dvla/core/ChecksumTool.java
@@ -0,0 +1,72 @@
+package uk.gov.dvla.core;
+
+
+import java.io.*;
+import java.security.MessageDigest;
+
+/**
+ * Class used to create checksum
+ */
+public class ChecksumTool {
+
+    /**
+     *
+     * @param fileInput
+     * @return
+     * @throws Exception
+     */
+    public static String generateSha256Checksum(File fileInput) throws Exception
+    {
+        FileInputStream fis = new FileInputStream(fileInput);
+        byte[] b = createChecksum(fis);
+        String result = "";
+        for (int i=0; i < b.length; i++) {
+            result +=
+                    Integer.toString( ( b[i] & 0xff ) + 0x100, 16).substring( 1 );
+        }
+        return result;
+    }
+
+    public static String generateSha256Checksum(String filename) throws Exception
+    {
+        return generateSha256Checksum(new File(filename));
+    }
+
+    public static boolean isValidChecksum(File fileInput, String checksum) throws Exception
+    {
+        return generateSha256Checksum(fileInput).equalsIgnoreCase(checksum);
+    }
+
+    public static boolean isValidChecksum(String filename, String checksum) throws Exception
+    {
+        return isValidChecksum(new File(filename), checksum);
+    }
+
+    private static byte[] createChecksum(FileInputStream fileInputStream) throws Exception
+    {
+        byte[] buffer = new byte[1024];
+        MessageDigest complete = MessageDigest.getInstance("SHA-256");
+        int numRead;
+        do {
+            numRead = fileInputStream.read(buffer);
+            if (numRead > 0) {
+                complete.update(buffer, 0, numRead);
+            }
+        } while (numRead != -1);
+        fileInputStream.close();
+        return complete.digest();
+    }
+
+    public static String readShaChecksumFile(String filePath) throws Exception {
+        FileInputStream fin = new FileInputStream(filePath);
+        BufferedReader myInput = new BufferedReader(new InputStreamReader(fin));
+        StringBuilder sb = new StringBuilder();
+        String thisLine = null;
+        while ((thisLine = myInput.readLine()) != null) {
+            sb.append(thisLine);
+        }
+
+        return sb.toString();
+    }
+
+}
---
+++ core/src/test/java/uk/gov/dvla/core/TestChecksumTool.java
@@ -0,0 +1,43 @@
+package uk.gov.dvla.core;
+
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class TestChecksumTool {
+
+    @Test
+    public void testChecksumCreation() throws Exception
+    {
+        String aTextFilePath = this.getClass().getClassLoader().getResource("a.txt").getFile();
+        String aTextShaFilePath = this.getClass().getClassLoader().getResource("a.txt.sha").getFile();
+        String generatedChecksum = ChecksumTool.generateSha256Checksum(aTextFilePath);
+        Assert.assertEquals("Checksum does not match expected", readFileContents(aTextShaFilePath), generatedChecksum);
+    }
+
+    @Test
+    public void testChecksumComparison() throws Exception
+    {
+        String aTextFilePath = this.getClass().getClassLoader().getResource("a.txt").getFile();
+        String aTextShaFilePath = this.getClass().getClassLoader().getResource("a.txt.sha").getFile();
+        Assert.assertTrue(ChecksumTool.isValidChecksum(aTextFilePath, readFileContents(aTextShaFilePath)));
+    }
+
+    private String readFileContents(String filePath) throws Exception
+    {
+        FileInputStream fin =  new FileInputStream(filePath);
+        BufferedReader myInput = new BufferedReader(new InputStreamReader(fin));
+        StringBuilder sb = new StringBuilder();
+        String thisLine = null;
+        while ((thisLine = myInput.readLine()) != null) {
+            sb.append(thisLine);
+        }
+
+        return sb.toString();
+    }
+}
