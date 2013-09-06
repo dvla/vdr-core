3a441bfb9b380c84326572788f2ca414b79d9dda
Ben Powell <blpowell@gmail.com>
2013-09-06 11:13:53 +0100

    Cleaned up PortalDTO

--- domain/src/main/java/uk/gov/dvla/domain/portal/portalDTO.java
+++ domain/src/main/java/uk/gov/dvla/domain/portal/portalDTO.java
@@ -315,7 +315,7 @@ public class PortalDTO {
 
     }
 
-    public static class Licence {
+    public static class     Licence {
 
         private String currentIssueNum;
         private Date validFrom;
@@ -582,7 +582,6 @@ public class PortalDTO {
 
     public static class OtherSentence {
         private String code;
-        private String name;
         private String duration;
 
         public String getCode() {
@@ -591,14 +590,6 @@ public class PortalDTO {
 
         public void setCode(String code) {
             this.code = code;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
         }
 
         public String getDuration() {
@@ -658,10 +649,6 @@ public class PortalDTO {
         private String entitlementType;
         private String statusType;
         private Date testPassDate;
-        private Boolean extended;
-        private Boolean harmonised;
-        private Boolean automatic;
-        private Boolean lessThanEqual25kw;
 
         public Date getExpiryDate() {
             Integer unclaimedTestPassValidityInMonths = DomainConfiguration.getInstance().getUnclaimedTestPassValidity();
@@ -699,38 +686,6 @@ public class PortalDTO {
 
         public void setTestPassDate(Date testPassDate) {
             this.testPassDate = testPassDate;
-        }
-
-        public Boolean getExtended() {
-            return extended;
-        }
-
-        public void setExtended(Boolean extended) {
-            this.extended = extended;
-        }
-
-        public Boolean getHarmonised() {
-            return harmonised;
-        }
-
-        public void setHarmonised(Boolean harmonised) {
-            this.harmonised = harmonised;
-        }
-
-        public Boolean getAutomatic() {
-            return automatic;
-        }
-
-        public void setAutomatic(Boolean automatic) {
-            this.automatic = automatic;
-        }
-
-        public Boolean getLessThanEqual25kw() {
-            return lessThanEqual25kw;
-        }
-
-        public void setLessThanEqual25kw(Boolean lessThanEqual25kw) {
-            this.lessThanEqual25kw = lessThanEqual25kw;
         }
     }
 
