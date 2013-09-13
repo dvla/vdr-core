cf282ef3052c1128ef954fe89f6d7e0acfe867b4
Radek Tkaczyk <r.tkaczyk@kainos.com>
2013-09-13 11:17:38 +0100

    added tests for real this time

--- core/src/test/java/uk/gov/dvla/core/TestValidators.java
+++ core/src/test/java/uk/gov/dvla/core/TestValidators.java
@@ -1,11 +1,33 @@
 package uk.gov.dvla.core;
 
-/**
- * Created with IntelliJ IDEA.
- * User: radekt
- * Date: 13/09/2013
- * Time: 10:49
- * To change this template use File | Settings | File Templates.
- */
+
+import junit.framework.Assert;
+import org.junit.Test;
+
 public class TestValidators {
+
+    @Test
+    public void testDlnValidators() {
+        String validDlns[] = {
+                "LEESH702100C99DP", "KITSO802192GT2BD", "T9999812208B99DG",
+                "TONY9810121MS2DB", "TYPEO606158A99QQ", "EXXXX811260EA9RE",
+                "e9999811260EA9Re", "JAM99711268KA9OP" };
+
+        String invalidDlns[] = {
+                "LEE999702100C99DP", "KITSO802192GT1BD", "99999812208B99DG",
+                "TON9810121MS2DB", "TYPE9O606158A99QQ", "EXXXX81T260EA9RE",
+                "e9999811260E89Re", "JAM98711268KA90P" };
+
+        for (String dln : validDlns) {
+            Assert.assertTrue(
+                    String.format("DLN [%s] incorrectly considered invalid", dln),
+                    Validators.validateDln(dln));
+        }
+
+        for (String dln : invalidDlns) {
+            Assert.assertFalse(
+                    String.format("DLN [%s] incorrectly considered valid", dln),
+                    Validators.validateDln(dln));
+        }
+    }
 }
