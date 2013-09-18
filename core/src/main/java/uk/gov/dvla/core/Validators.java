0d79f744308ef8a2cbcc30ca373e799cfb19a4c5
Radek Tkaczyk <r.tkaczyk@kainos.com>
2013-09-18 12:03:54 +0100

    style fixes

--- core/src/main/java/uk/gov/dvla/core/Validators.java
+++ core/src/main/java/uk/gov/dvla/core/Validators.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public final class Validators {
     /*
-        The regular expression below is the GDS official regex for matching DLN (info from lmlynik).
+        The regular expression below supposedly is the GDS official regex for matching DLN.
         It is not very well written. It unnecessarily uses alternatives and creates redundant capturing groups.
         Consider:
             "^(?=\\w{16}$)[A-Za-z]{1,5}9{0,4}[0-9]{6}[A-Za-z][A-Za-z9][A-Za-z2-9][A-Za-z0-9]{2}"
