3b54aa75cb1a300e3794889c95cc10e57f9adace
Emer Rooney <e.rooney@kainos.com>
2013-09-19 10:38:38 +0100

    Code updated to move the messages to from the ServiceResult

--- api/src/main/java/uk/gov/dvla/services/filter/FilterService.java
+++ api/src/main/java/uk/gov/dvla/services/filter/FilterService.java
@@ -1,12 +1,8 @@
 package uk.gov.dvla.services.filter;
 
 import uk.gov.dvla.domain.ServiceResult;
-import java.util.UUID;
 
 public interface FilterService<T>
 {
-    //public ServiceResult<T> filter(UUID id, T objectToFilter, String Context);
-
     public ServiceResult<T> filter(T objectToFilter, String Context);
-
 }
--- api/src/main/java/uk/gov/dvla/services/transform/TransformService.java
+++ api/src/main/java/uk/gov/dvla/services/transform/TransformService.java
@@ -1,5 +1,9 @@
 package uk.gov.dvla.services.transform;
 
+import uk.gov.dvla.domain.Message;
+
+import java.util.List;
+
 public interface TransformService<T, U> {
 
     public U transform(T objectToTransform);
