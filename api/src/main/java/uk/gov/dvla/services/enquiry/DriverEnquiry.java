4de9aeee1698cc24af6abc653920ba072924874e
Lukasz Mlynik <l.mlynik@kainos.com>
2013-09-30 14:23:36 +0100

    Merge remote-tracking branch 'origin/auth' into auth-remerge
    Conflicts:
    	iep-customer-portal/conf/routes
            many many others

--- api/src/main/java/uk/gov/dvla/services/enquiry/DriverEnquiry.java
+++ api/src/main/java/uk/gov/dvla/services/enquiry/DriverEnquiry.java
@@ -4,13 +4,32 @@ import uk.gov.dvla.domain.Driver;
 import uk.gov.dvla.domain.Person;
 import uk.gov.dvla.services.ManagedService;
 
-public interface DriverEnquiry extends ManagedService
-{
-    public static final String CUSTOMER_PORTAL_SERVICE_ENDPOINT = "/driver/";
+public interface DriverEnquiry extends ManagedService {
     public static final String MIB_SERVICE_ENDPOINT = "/iiadd/api/v1/driver";
     public static final String MIB_SERVICE_VERSION_NUMBER = "1";
+    public static final String CUSTOMER_PORTAL_SERVICE_ENDPOINT = "/driver/";
+    public static final String DLN_SEARCH = "dlnSearch";
+    public static final String PERSONAL_DETAILS_SEARCH = "personalDetailsSearch";
     public static final String CUSTOMER_PORTAL = "customer.portal";
     public static final String MIB = "mib";
+    public static final String NINO_PARAM = "n";
+
+
+    public static final String DLN_PARAM = "dln";
+    public static final String ENQUIRY_ID_PARAM = "guid";
+
+    public static final String FORENAME_PARAM = "fn";
+    public static final String SURNAME_PARAM = "sn";
+    public static final String DOB_PARAM = "d";
+    public static final String GENDER_PARAM = "g";
+    public static final String POSTCODE_PARAM = "pc";
+
+    public static final String VEHICLE_REGISTRATION_MARK_PARAM = "vrm";
+
+    public static final String SERVICE_TYPE_PARAM = "type";
+
+    public static final String CONTACT_CHANNEL_PARAM = "cc";
+    public static final String ENQUIRY_REASON_PARAM = "er";
 
     public Driver get(String dln);
 
