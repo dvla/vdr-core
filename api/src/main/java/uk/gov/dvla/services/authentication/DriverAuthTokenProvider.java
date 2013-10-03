60eac7805343512ee5666d9fb6f27b2493c4064c
Lukasz Mlynik <l.mlynik@kainos.com>
2013-10-03 15:21:32 +0100

    Customer portal integration with Auth service gateway. WIP

---
+++ api/src/main/java/uk/gov/dvla/services/authentication/DriverAuthTokenProvider.java
@@ -0,0 +1,12 @@
+package uk.gov.dvla.services.authentication;
+
+import uk.gov.dvla.domain.authentication.DriverAuthToken;
+
+import java.util.Date;
+import java.util.List;
+
+public interface DriverAuthTokenProvider {
+    DriverAuthToken findDriverAuthTokenByDln(String dln);
+
+    List<DriverAuthToken> findDriverAuthTokenByPersonalDetails(List<String> forenames, String surname, Date dob, int gender, String postCode);
+}
--- api/src/main/java/uk/gov/dvla/services/datastore/DriverDatastore.java
+++ api/src/main/java/uk/gov/dvla/services/datastore/DriverDatastore.java
@@ -2,14 +2,15 @@ package uk.gov.dvla.services.datastore;
 
 import uk.gov.dvla.domain.Driver;
 import uk.gov.dvla.services.ManagedService;
+import uk.gov.dvla.services.authentication.DriverAuthTokenProvider;
+
 import java.util.Date;
 import java.util.List;
 
-public interface DriverDatastore extends Datastore, ManagedService
-{
-	public Driver findByDln(String dln);
-	
-	public List<Driver> findByPersonalDetails(List<String> forenames, String surname, Date dob, int gender, String postCode);
+public interface DriverDatastore extends Datastore, DriverAuthTokenProvider, ManagedService {
+    public Driver findByDln(String dln);
+
+    public List<Driver> findByPersonalDetails(List<String> forenames, String surname, Date dob, int gender, String postCode);
 
     public void add(Driver driver);
 
