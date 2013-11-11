fe2b2b7273c67f84f6b7d3355a1e7fe5b956193c
Neil Grattan <n.grattan@kainos.com>
2013-11-11 12:34:00 +0000

    Updated document checking service to adhere to latest schema spec

--- api/src/main/java/uk/gov/dvla/services/enquiry/DriverEnquiry.java
+++ api/src/main/java/uk/gov/dvla/services/enquiry/DriverEnquiry.java
@@ -9,6 +9,7 @@ public interface DriverEnquiry extends ManagedService {
     public static final String MIB_SERVICE_VERSION_NUMBER = "1";
     public static final String CUSTOMER_PORTAL_SERVICE_ENDPOINT = "/driver/";
     public static final String HEALTH_CHECK_ENDPOINT = "/healthcheck";
+    public static final String DCS_SERVICE_ENDPOINT = "/search";
     public static final String DLN_SEARCH = "dlnSearch";
     public static final String PERSONAL_DETAILS_SEARCH = "personalDetailsSearch";
     public static final String CUSTOMER_PORTAL = "customer.portal";
