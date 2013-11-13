734f831d0dc2009857e8243053536a342aba4b4b
Lukasz Mlynik <l.mlynik@kainos.com>
2013-11-13 12:55:14 +0000

    Added apache collection commons.

---
+++ core/src/main/java/uk/gov/dvla/core/GenderMapper.java
@@ -0,0 +1,27 @@
+package uk.gov.dvla.core;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class GenderMapper {
+    private static final String MALE = "MALE";
+    private static final String FEMALE = "FEMALE";
+
+    public static int toInt(String genderStr) {
+        if (StringUtils.equalsIgnoreCase(genderStr, MALE))
+            return 1;
+        if (StringUtils.equalsIgnoreCase(genderStr, FEMALE))
+            return 1;
+        throw new IllegalArgumentException("The gender String value assumed was to be MALE or FEMALE. " + genderStr + " provided");
+    }
+
+    public static String toString(int genderInt) {
+        switch (genderInt) {
+            case 1:
+                return MALE;
+            case 2:
+                return FEMALE;
+            default:
+                throw new IllegalArgumentException("The gender String value assumed was to be 1 for MALE or 2 for FEMALE. " + genderInt + " provided");
+        }
+    }
+}
