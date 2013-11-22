81c764b36c364ad0ac65ef332440e0886c6866e5
Lukasz Mlynik <l.mlynik@kainos.com>
2013-11-22 12:33:20 +0000

    Updated matching data set per newest news.

--- core/src/main/java/uk/gov/dvla/core/GenderMapper.java
+++ core/src/main/java/uk/gov/dvla/core/GenderMapper.java
@@ -5,12 +5,16 @@ import org.apache.commons.lang3.StringUtils;
 public class GenderMapper {
     private static final String MALE = "MALE";
     private static final String FEMALE = "FEMALE";
+    private static final String NOT_SPECIFIED = "NOT_SPECIFIED";
 
     public static int toInt(String genderStr) {
         if (StringUtils.equalsIgnoreCase(genderStr, MALE))
             return 1;
         if (StringUtils.equalsIgnoreCase(genderStr, FEMALE))
             return 2;
+        if (StringUtils.equalsIgnoreCase(genderStr, NOT_SPECIFIED))
+            return 9;
+
         throw new IllegalArgumentException("The gender String value assumed was to be MALE or FEMALE. " + genderStr + " provided");
     }
 
@@ -20,6 +24,8 @@ public class GenderMapper {
                 return MALE;
             case 2:
                 return FEMALE;
+            case 9:
+                return NOT_SPECIFIED;
             default:
                 throw new IllegalArgumentException("The gender String value assumed was to be 1 for MALE or 2 for FEMALE. " + genderInt + " provided");
         }
