fdbb17fd9aa2b516fdaa79eeccc1e57f0be2bd8e
Andrew Gorton <a.gorton@kainos.com>
2013-12-17 17:02:32 +0000

    More updates

--- messages/src/main/scala/uk/gov/dvla/messages/WebformEnquiries.scala
+++ messages/src/main/scala/uk/gov/dvla/messages/WebformEnquiries.scala
@@ -5,11 +5,11 @@ import org.joda.time.DateTime
 import scala.collection.immutable.Map
 
 /**
- * A record suppression enquiry message.
+ * A record suppression enquiry DTO.
  *
  * This is simply the data from the webform prior to enrichment.
  *
- * @param formData The form fields in a key-value map.
+ * @param formDataParam The form fields in a key-value map.
  */
 class RecordSuppressionEnquiryMessage(formDataParam: Map[String, String])
   extends Message with Serializable {
@@ -20,6 +20,9 @@ class RecordSuppressionEnquiryMessage(formDataParam: Map[String, String])
    */
   val timestamp = DateTime.now
 
+  /**
+   * The contents of our form data, in a map of [FieldName: String, Data: String].
+   */
   val formData = formDataParam
 }
 
