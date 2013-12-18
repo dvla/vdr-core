96dfdf9a4527552eb652e949c0bb7c2b6a89e1d0
Andrew Gorton <a.gorton@kainos.com>
2013-12-18 09:56:17 +0000

    Initial enrichment component created

--- messages/src/main/scala/uk/gov/dvla/messages/WebformEnquiries.scala
+++ messages/src/main/scala/uk/gov/dvla/messages/WebformEnquiries.scala
@@ -3,6 +3,7 @@ package uk.gov.dvla
 import uk.gov.dvla.servicebus.core.Message
 import org.joda.time.DateTime
 import scala.collection.immutable.Map
+import scala.xml.{Null, Text, Attribute, NodeSeq}
 
 /**
  * A record suppression enquiry DTO.
@@ -21,8 +22,29 @@ class RecordSuppressionEnquiryMessage(formDataParam: Map[String, String])
   val timestamp = DateTime.now
 
   /**
-   * The contents of our form data, in a map of [FieldName: String, Data: String].
+   * The contents of our form data, in a map of [fieldName: String, data: String].
    */
   val formData = formDataParam
+
+  /**
+   * Serializes this object to an XML representation.
+   *
+   * @return XML nodes representing this class.
+   */
+  def toXml: NodeSeq = {
+    val fields = getClass.getDeclaredFields
+    fields foreach {
+      _.setAccessible(true)
+    }
+    val data = fields map {
+      f => f.getName -> f.get(this).toString
+    }
+    val xmlTag = <a/>.copy(label = getClass.getSimpleName)
+
+    (xmlTag /: data) {
+      case (rec, (name, value)) =>
+        rec % Attribute(None, name, Text(value), Null)
+    }
+  }
 }
 
