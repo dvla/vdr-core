11ab4cc8e033e2d4028cf154d1b967cd4fa16608
Lukasz Mlynik <l.mlynik@kainos.com>
2014-01-25 11:20:15 +0000

    Enrichment service now consumes JSON messages

--- messages/src/main/scala/uk/gov/dvla/messages/WebformEnquiries.scala
+++ messages/src/main/scala/uk/gov/dvla/messages/WebformEnquiries.scala
@@ -16,34 +16,23 @@ import uk.gov.dvla.messages.XmlMessageSerialization
  * @param enrichedData Key-value pairing of enriched data fields.
  * @param baseMessage The RecordSuppressionEnquiry message to copy form data from.
  */
-class RecordSuppressionEnrichedMessage(val suppressionReason: String,
-                                       val enrichedData: Map[String, String],
-                                       baseMessage: RecordSuppressionEnquiryMessage)
+case class RecordSuppressionEnrichedMessage(suppressionReason: String,
+                                            enrichedData: Map[String, String],
+                                            baseMessage: RecordSuppressionEnquiryMessage)
   extends Message with XmlMessageSerialization with Serializable {
 
   val timestamp = baseMessage.timestamp
-  val formData = baseMessage.formData
+  def formData = baseMessage.formData
 }
 
 /**
  * A record suppression enquiry DTO.
  *
  * This is simply the data from the webform prior to enrichment.
- *
- * @param formDataParam The form fields in a key-value map.
+ * @param formData The form fields in a key-value map.
  */
-class RecordSuppressionEnquiryMessage(formDataParam: Map[String, String])
+case class RecordSuppressionEnquiryMessage(formData: Map[String, String])
   extends Message with XmlMessageSerialization with Serializable {
-
-  /**
-   * Keep track of form submission times, and allows us to identify performance issues and
-   * end to end response times.
-   */
   val timestamp = DateTime.now
-
-  /**
-   * The contents of our form data, in a map of [fieldName: String, data: String].
-   */
-  val formData = formDataParam
 }
 
