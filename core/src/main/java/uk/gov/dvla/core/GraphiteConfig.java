b8194d2c4cb5551e93d9da0a35f78651fc04fb32
David Breeze <david.breeze@dvla.gsi.gov.uk>
2014-01-30 17:38:33 +0000

    First run at finding all running Graphite

---
+++ core/src/main/java/uk/gov/dvla/core/GraphiteConfig.java
@@ -0,0 +1,23 @@
+package uk.gov.dvla.core;
+
+import javax.validation.constraints.NotNull;
+
+public class GraphiteConfig {
+    @NotNull
+    public String status = "off";
+
+    @NotNull
+    public String server = "localhost";
+
+    @NotNull
+    public Integer port = 2003;
+
+    @NotNull
+    public Integer timeoutSecs = 5;
+
+    @Override
+    public String toString() {
+        return String.format("GraphiteConfiguration: Status $s Server %s  Port: %s DB: %s Timeout(secs): %s", status, server, port, timeoutSecs);
+    }
+
+}
