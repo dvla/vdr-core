1c632c9b0f8ad566afded0a608ddac458ac8ae4f
Neil Grattan <n.grattan@kainos.com>
2014-04-11 14:49:25 +0100

    Fixed DCS to not return false when an optional matching rule fails, just audit the failure for MI.

--- api/src/main/java/uk/gov/dvla/services/ida/dcs/DriverMatcher.java
+++ api/src/main/java/uk/gov/dvla/services/ida/dcs/DriverMatcher.java
@@ -2,7 +2,8 @@ package uk.gov.dvla.services.ida.dcs;
 
 import com.google.common.base.Optional;
 import java.util.List;
+import java.util.Map;
 
 public interface DriverMatcher<A, B> {
-    public Optional<List<String>> match(A matchRequest, B matchResponse);
+    public Optional<Map<String, Boolean>> match(A matchRequest, B matchResponse);
 }
--- api/src/main/java/uk/gov/dvla/services/specifications/ISpecification.java
+++ api/src/main/java/uk/gov/dvla/services/specifications/ISpecification.java
@@ -1,6 +1,7 @@
 package uk.gov.dvla.services.specifications;
 
 public interface ISpecification<A, B> {
+    public boolean isOptional();
     public String getSpecificationName();
     public boolean isSatisfiedBy(A compare1, B compare2);
 }
