78c7ed35f9e3437c4acb82fe57853debf9930010
Neil Grattan <n.grattan@kainos.com>
2014-04-15 11:06:30 +0100

    Fixed Dependency from matcher service on DCS specification logic

--- api/src/main/java/uk/gov/dvla/services/ida/dcs/DriverMatcher.java
+++ api/src/main/java/uk/gov/dvla/services/ida/dcs/DriverMatcher.java
@@ -1,7 +1,6 @@
 package uk.gov.dvla.services.ida.dcs;
 
 import com.google.common.base.Optional;
-import java.util.List;
 import java.util.Map;
 
 public interface DriverMatcher<A, B> {
--- api/src/main/java/uk/gov/dvla/services/ida/matcher/DriverMatcher.java
+++ api/src/main/java/uk/gov/dvla/services/ida/matcher/DriverMatcher.java
@@ -1,7 +1,8 @@
 package uk.gov.dvla.services.ida.matcher;
 
-/**
- * Created by neilg on 15/04/2014.
- */
-public class DriverMatcher {
+import com.google.common.base.Optional;
+import java.util.List;
+
+public interface DriverMatcher<A, B> {
+    public Optional<List<String>> match(A matchRequest, B matchResponse);
 }
